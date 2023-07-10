1a865690d7c0eefb3110c5a6233b527ced614dfe
S246now <odalisse.arguello@udla.edu.ec>
2023-07-10 12:14:13 -0500

    created structure and some initial files and components

---
+++ components/listarVentas.js
@@ -0,0 +1,9 @@
+function ListarVentas() {
+    return(
+        <div>
+            <h1>Listado de Ventas junto con su respectivo Cliente</h1>
+        </div>
+    )
+}
+
+export default ListarVentas;
